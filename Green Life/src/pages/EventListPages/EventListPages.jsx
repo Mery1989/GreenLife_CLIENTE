9a0202f0e6820d8d5ec88bde9d22098903034d52
AntonioMelo7 <a.meloleon@gmail.com>
2024-04-28 11:21:06 +0200

    cambio todo ok

--- Green Life/src/pages/EventListPages/EventListPages.jsx
+++ Green Life/src/pages/EventListPages/EventListPages.jsx
@@ -1,7 +1,6 @@
 import { Container } from "react-bootstrap";
 import BannerEvent from "../../components/Banner/EventBanner";
 import EventList from "../../components/EventList/EventList";
-import ModalEvent from "../../components/ModalEvent/ModalEvent";
 
 const EventListPage = () => {
 
