64de6f2d702eb61e540ffd5a5be49b29c4e8ff0b
AntonioMelo7 <a.meloleon@gmail.com>
2024-04-28 11:42:38 +0200

    Revert "cambio todo ok"
    This reverts commit 9a0202f0e6820d8d5ec88bde9d22098903034d52.

--- Green Life/src/pages/EventListPages/EventListPages.jsx
+++ Green Life/src/pages/EventListPages/EventListPages.jsx
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import BannerEvent from "../../components/Banner/EventBanner";
 import EventList from "../../components/EventList/EventList";
+import ModalEvent from "../../components/ModalEvent/ModalEvent";
 
 const EventListPage = () => {
 
