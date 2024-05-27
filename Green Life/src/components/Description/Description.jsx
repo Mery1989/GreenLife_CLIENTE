5ee883d2b2f42d847bf2161d7e3fabf6fc7841d8
AntonioMelo7 <a.meloleon@gmail.com>
2024-05-27 20:33:13 +0200

    CAMBIOS FINALES

--- Green Life/src/components/Description/Description.jsx
+++ Green Life/src/components/Description/Description.jsx
@@ -13,30 +13,30 @@ function Description() {
                 <Row className='column'>
                    <Col sm={12} md={4}>
                         <h4 className='titlesDescription'>Historia y Orígenes</h4>
-                        <p className='contentDescription'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </p>
+                        <p className='contentDescription'>GreenLife ha nacido para mejorar las costumbres de todas las personas y hacer un mundo mejor. </p>
                     </Col>
                    <Col sm={12} md={4}>
                         <h4 className='titlesDescription'>Misión y Visión</h4>
-                        <p className='contentDescription'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </p>
+                        <p className='contentDescription'>Mejorar las costumbres ambientales de las personas promoviendo una vida sostenible. </p>
                     </Col>
                    <Col sm={12} md={4}>
                         <h4 className='titlesDescription'>Valores Fundamentales</h4>
-                        <p className='contentDescription'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </p>
+                        <p className='contentDescription'>Con una serie de pequeñas acciones entre todos podemos hacer una civilización mas ecológica. </p>
                     </Col>
                 </Row>
 
                 <Row className='column'>
                    <Col sm={12} md={4}>
                         <h4 className='titlesDescription'>Equipo y Fundadores</h4>
-                        <p className='contentDescription'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </p>
+                        <p className='contentDescription'>Somos cuatro personas trabajadoras buscando crear un mundo mejor. </p>
                     </Col>
                    <Col sm={12} md={4}>
                         <h4 className='titleGreen titlesDescription'>Compromiso con la Sostenibilidad</h4>
-                        <p className='contentDescription'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </p>
+                        <p className='contentDescription'>Mejoraremos todas las costumbres de nuestros usuarios provocando una mejora en su huella de carbono. </p>
                     </Col>
                    <Col sm={12} md={4}>
                         <h4 className='titlesDescription'>Colaboraciones y Alianzas</h4>
-                        <p className='contentDescription'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </p>
+                        <p className='contentDescription'>Empresas con el deseo de mostrar al mundo que buscan ser mejores ambientalmente hablando y que quieren participar en el proceso. </p>
                     </Col>
                 </Row>
 
